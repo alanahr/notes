9b5660b0917142eec8b7dcf6787e5844e45f6610
AAR <25590997+alanahr@users.noreply.github.com>
2026-02-18 00:33:55 -0600

    Add stored procedures for introspection in PostgreSQL

---
+++ postgresql_stored_procedures_for_introspection.sql
@@ -0,0 +1,17 @@
+create or replace function get_col_names(rel regclass, cols int2[])
+returns text language sql as $$
+    select string_agg(attname, ', ' order by ordinality)
+    from pg_attribute,
+    unnest(cols) with ordinality
+    where attrelid = rel
+    and attnum = unnest
+$$;
+create or replace function get_col_types(rel regclass, cols int2[])
+returns text language sql as $$
+    select string_agg(typname, ', ' order by ordinality)
+    from pg_attribute a
+    join pg_type t on t.oid = atttypid,
+    unnest(cols) with ordinality
+    where attrelid = rel
+    and attnum = unnest
+$$;
